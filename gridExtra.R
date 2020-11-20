865e346130f18da762094686a9e3e8dd422ebdbb
Taylor Spaulding <TSpaulding@esassoc.com>
2020-11-20 15:04:01 -0800

    additional layouts

--- gridExtra.R
+++ gridExtra.R
@@ -7,6 +7,7 @@
 # The following script is designed to introduce users to more 
 # advanced aspects of gridEXTRA
 
+library(tidyverse)
 library(ggplot2)
 library(gridExtra)
 
@@ -94,9 +95,43 @@ grid.arrange(mylegend, arrangeGrob(atk_def + theme(legend.position = "none"), #D
                          atk_spatk + theme(legend.position = "none"),
                          ncol = 2, nrow = 2),                         
              nrow = 2, heights = c(1, 30),
-             bottom = "Attack", padding = unit(1,"mm"))
+             bottom = "Attack")
 
-# looks a little spacey, so we tighten up
-mytheme <- gridExtra::ttheme_default(
-  core = list(padding=unit(c(1, 1), "mm"))
-)             
+# Opportunities here are endless
+
+# Complicated/ Interesting plots using custom matrix layouts
+
+layout = rbind(c(1,1,1,1,1,NA),
+               c(2,2,2,2,2,3),
+               c(2,2,2,2,2,3),
+               c(2,2,2,2,2,3),
+               c(2,2,2,2,2,3),
+               c(2,2,2,2,2,3))
+
+atk_histo <- ggplot(data = poke_mod, mapping = aes(x = Attack, fill = Type.1))+
+  geom_histogram()+
+  theme(
+    legend.position = "bottom",
+    axis.text.x = element_blank(),
+    axis.ticks.x = element_blank(),
+    axis.title.x = element_blank()
+  )
+
+def_histo <- ggplot(data = poke_mod, mapping = aes(x = Defense, fill = Type.1))+
+  geom_histogram()+
+  theme(
+    legend.position = "none",
+    axis.text.y = element_blank(),
+    axis.ticks.y = element_blank(),
+    axis.title.y = element_blank()
+  )+
+  coord_flip()
+
+mylegend<-g_legend(atk_histo)
+
+grid.arrange(arrangeGrob(atk_histo + theme(legend.position = "none"), 
+                         atk_def + theme(legend.position = "none") + labs(x = "Attack"), 
+                         def_histo, 
+                         layout_matrix = layout),
+             mylegend, 
+             nrow = 2, heights = c(30,1))
